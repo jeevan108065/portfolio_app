19976ed4dfae69d9d18b2ec39b4d5b5e206cc07c
jeevan108065 <pjeevankumar225@gmail.com>
2023-02-04 00:57:03 +0530

    ,

--- src/FaceComponent/FaceComponent.js
+++ src/FaceComponent/FaceComponent.js
@@ -1,10 +1,13 @@
 import react from "react";
 import "./FaceComponent.css";
+import "../Static/images/ShivaMain.jpg";
 
 const FaceComponent = () => {
+  const mainShiva = require("../Static/images/ShivaMain.jpg");
   return (
     <div className="face">
       <div>Shiva Prasaad Pappala</div>
+      <img className="centreImage" src={mainShiva} width="180px"></img>
     </div>
   );
 };

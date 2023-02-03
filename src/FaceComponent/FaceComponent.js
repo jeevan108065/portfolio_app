370a5e88131bae091a00cf1a450f07b9065ab51d
jeevan108065 <pjeevankumar225@gmail.com>
2023-02-04 00:23:43 +0530

    baic

---
+++ src/FaceComponent/FaceComponent.js
@@ -0,0 +1,12 @@
+import react from "react";
+import "./FaceComponent.css";
+
+const FaceComponent = () => {
+  return (
+    <div className="face">
+      <div>Shiva Prasaad Pappala</div>
+    </div>
+  );
+};
+
+export default FaceComponent;
